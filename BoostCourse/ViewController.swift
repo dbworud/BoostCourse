2b4cb0662bfa864dbfbd92bda3f9df41c2b9ba44
JaekyungYou <youjk96@gmail.com>
2021-01-08 12:18:53 +0900

    feat: setup UI and IBOutlets

--- BoostCourse/ViewController.swift
+++ BoostCourse/ViewController.swift
@@ -5,15 +5,43 @@
 //  Created by jaekyung you on 2021/01/08.
 //
 
+import AVFoundation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    // MARK: Properties
+    var player: AVAudioPlayer!
+    var timer: Timer!
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var progressSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: Methods
+    
+    // Player 시작
+    
+    // Timer 시작
+    
+    // Tiemr 끝내기
+    
+    
+    // 
+    
+    
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        print("Tapped Button")
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("Slider value changed")
+    }
 }
 
